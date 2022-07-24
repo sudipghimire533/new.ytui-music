4c2f30de8b1da36d58dd4a51e99ee6e6ef2968cb
sudipghimire533 <sudipghimire533@gmail.com>
2022-07-24 20:37:30 +0545

    Add computer_for_child flag to skip re-computing if possible

--- layout/src/rect_computation.rs
+++ layout/src/rect_computation.rs
@@ -15,7 +15,7 @@ fn i_can_start_from(
 
         Some(parent) => {
             if !size_map.contains_key(&parent.item.identifier) {
-                compute_rect_for_item_tree(&parent, size_map, terminal_rect);
+                compute_rect(parent, size_map, terminal_rect, false);
             }
             let parent_rect = size_map
                 .get(&parent.item.identifier)
@@ -29,7 +29,7 @@ fn i_can_start_from(
                 .last()
                 .map(|s| {
                     if !size_map.contains_key(&s.item.identifier) {
-                        compute_rect_for_item_tree(s, size_map, terminal_rect);
+                        compute_rect(s, size_map, terminal_rect, false);
                     }
                     let sibling_rect = size_map
                         .get(&s.item.identifier)
@@ -50,6 +50,15 @@ pub fn compute_rect_for_item_tree(
     size_map: &mut HashMap<Identifier, Rect>,
     terminal_rect: &Rect,
 ) {
+    compute_rect(me, size_map, terminal_rect, true)
+}
+
+fn compute_rect(
+    me: &ItemTree,
+    size_map: &mut HashMap<Identifier, Rect>,
+    terminal_rect: &Rect,
+    compute_for_child: bool,
+) {
     let final_rect: Rect = match me.parent.as_ref() {
         // for root element it always fill the terminal_rect
         // this means that
@@ -61,7 +70,7 @@ pub fn compute_rect_for_item_tree(
 
         Some(parent) => {
             if !size_map.contains_key(&parent.item.identifier) {
-                compute_rect_for_item_tree(parent, size_map, terminal_rect);
+                compute_rect(parent, size_map, terminal_rect, false);
             }
             let parent_rect = size_map
                 .get(&parent.item.identifier)
@@ -89,8 +98,10 @@ pub fn compute_rect_for_item_tree(
     size_map.insert(me.item.identifier.clone(), final_rect);
 
     // Now also compute for all child
-    for child in me.childs.iter() {
-        compute_rect_for_item_tree(child, size_map, terminal_rect)
+    if compute_for_child {
+        for child in me.childs.iter() {
+            compute_rect(child, size_map, terminal_rect, true)
+        }
     }
 }
 
@@ -114,91 +125,61 @@ mod tests {
 
     #[test]
     fn test_rect_for_layout_file() {
+        use Identifier::Custom;
         let config_file_str = include_str!("../../layout-config/layout.json");
         let ui: UI = serde_json::from_str(config_file_str).unwrap();
         let mut size_map = HashMap::new();
 
         compute_rect_for_item_tree(&ui.item_root, &mut size_map, &TERMINAL_RECT);
 
-        let root = Rect {
-            x: 0,
-            y: 0,
-            height: 33,
-            width: 150,
-        };
+        #[rustfmt::skip]
+        let root = Rect { x: 0, y: 0, height: 33, width: 150 };
         assert_eq!(
             Some(&root),
-            size_map.get(&Identifier::Custom("things_starts_from_me".into()))
+            size_map.get(&Custom("things_starts_from_me".into()))
         );
 
-        let top_area = Rect {
-            x: 0,
-            y: 0,
-            height: 16,
-            width: 150,
-        };
-        assert_eq!(
-            Some(&top_area),
-            size_map.get(&Identifier::Custom("top_area".into()))
-        );
+        #[rustfmt::skip]
+        let top_area = Rect { x: 0, y: 0, height: 16, width: 150 };
+        assert_eq!(Some(&top_area), size_map.get(&Custom("top_area".into())));
 
-        let red_el_custom = Rect {
-            x: 0,
-            y: 16,
-            height: 5,
-            width: 150,
-        };
+        #[rustfmt::skip]
+        let red_el_custom = Rect { x: 0, y: 16, height: 5, width: 150 };
         assert_eq!(
             Some(&red_el_custom),
             size_map.get(&Identifier::Custom("red_element_custom".into())),
         );
 
-        let top_left = Rect {
-            x: 0,
-            y: 0,
-            height: 16,
-            width: 75,
-        };
+        #[rustfmt::skip]
+        let top_left = Rect { x: 0, y: 0, height: 16, width: 75 };
         assert_eq!(
             Some(&top_left),
             size_map.get(&Identifier::Custom("top_left".into()))
         );
 
-        let top_right = Rect {
-            x: 75,
-            y: 0,
-            height: 16,
-            width: 75,
-        };
+        #[rustfmt::skip]
+        let top_right = Rect { x: 75, y: 0, height: 16, width: 75 };
         assert_eq!(
             Some(&top_right),
             size_map.get(&Identifier::Custom("top_right".into()))
         );
 
-        let green_container = Rect {
-            x: 75,
-            y: 0,
-            height: 5,
-            width: 75,
-        };
+        #[rustfmt::skip]
+        let green_container = Rect { x: 75, y: 0, height: 5, width: 75 };
         assert_eq!(
             Some(&green_container),
             size_map.get(&Identifier::Custom("green_container".into()))
         );
 
-        let yellow_container = Rect {
-            y: 5,
-            ..green_container
-        };
+        #[rustfmt::skip]
+        let yellow_container = Rect { y: 5, ..green_container };
         assert_eq!(
             Some(&yellow_container),
             size_map.get(&Identifier::Custom("yellow_container".into()))
         );
 
-        let blue_container = Rect {
-            y: 10,
-            ..yellow_container
-        };
+        #[rustfmt::skip]
+        let blue_container = Rect { y: 10, ..yellow_container };
         assert_eq!(
             Some(&blue_container),
             size_map.get(&Identifier::Custom("blue_container".into()))
