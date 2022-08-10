ae5642de20ab3d8b0ad31ac884691cb659b0911b
sudipghimire533 <sudipghimire533@gmail.com>
2022-08-10 12:26:45 +0545

    Remove unused struct

--- user-config/src/preferences/pane.rs
+++ user-config/src/preferences/pane.rs
@@ -1,5 +1,3 @@
-use crate::styles::Style;
-
 pub struct PanePreferences {
     /// How many number of list item to show
     /// when we are fetching result from local source
@@ -7,8 +5,4 @@ pub struct PanePreferences {
     /// How many number of item in list to show
     /// when fetching result from remote source
     pub remote_result_count: u8,
-    /// base style for list item
-    pub base_style: Style,
-    /// highlight style for list item
-    pub highlight_style: Style,
 }
--- user-config/src/styles/mod.rs
+++ user-config/src/styles/mod.rs
@@ -1,7 +1,1 @@
 pub mod color;
-
-pub struct Style {
-    fg: Option<color::RGB>,
-    bg: Option<color::RGB>,
-    modifier: tui::style::Modifier,
-}
