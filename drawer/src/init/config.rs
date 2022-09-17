27b11244f6913bc972dffcbff9e22418b5f28055
sudipghimire533 <sudipghimire533@gmail.com>
2022-09-17 09:53:56 +0545

    Error handeling while getting preference dir

--- drawer/src/init/config.rs
+++ drawer/src/init/config.rs
@@ -3,14 +3,14 @@ use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use user_config::Config;
 
-fn get_config_path() -> PathBuf {
-    let mut config_dir = dirs::preference_dir().unwrap();
+fn get_config_path() -> Result<PathBuf, &'static str> {
+    let mut config_dir = dirs::preference_dir().ok_or("Cannot get user preference dir")?;
     config_dir = config_dir.join("ytui_music").join("config.json");
-    config_dir
+    Ok(config_dir)
 }
 
 pub fn default_config_source() -> Result<BufReader<File>, String> {
-    let config_path = get_config_path();
+    let config_path = get_config_path().map_err(|e| format!("While getting config path: {e:?}"))?;
     let file = File::open(config_path).map_err(|e| format!("While opening config file: {e:?}"))?;
     Ok(BufReader::new(file))
 }
